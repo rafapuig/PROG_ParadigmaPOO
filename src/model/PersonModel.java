bbb208932606f40a2e2c1528269e326332b9e157
Rafa Puig <rafapuig.profe@gmail.com>
2025-01-06 01:32:02 +0100

    Model

---
+++ src/model/PersonModel.java
@@ -0,0 +1,43 @@
+package model;
+
+public class PersonModel {
+
+    private String name;
+    private int age;
+    private boolean married;
+
+    public PersonModel() {
+        this("Desconocido", 0, false);
+    }
+
+    public PersonModel(String name, int age, boolean married) {
+        this.name = name;
+        this.age = age;
+        this.married = married;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+
+}
