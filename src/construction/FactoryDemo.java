d2740e9dc3811575fd46edbe6faaa7639240f43f
Rafa Puig <rafapuig.profe@gmail.com>
2025-01-06 02:21:07 +0100

    Factoria

---
+++ src/construction/FactoryDemo.java
@@ -0,0 +1,34 @@
+package construction;
+
+class Person {
+
+    private String name;
+    private int age;
+
+    private Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    // Método factoría estática
+    public static Person create(String name, int age) {
+        return new Person(name, age);
+    }
+}
+
+public class FactoryDemo {
+
+    public static void main(String[] args) {
+
+        Person perico = Person.create("Perico Palotes", 45);
+
+        System.out.println(perico.getName());
+        System.out.println(perico.getAge());
+    }
+}
